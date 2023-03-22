2ffcd8a1326b2cba4179a4dfc5f7fb7e24afc6d5
Juan <juan.0403.linux@gmail.com>
2023-03-21 23:15:04 -0600

    Modulo Principal

--- Practica-1/main.py
+++ Practica-1/main.py
@@ -16,41 +16,21 @@ class Main:
         self.lbl2 = ttk.Label(self.frm, text="Escoja una opción:")
         self.lbl2.grid(column=0, row=1)
         self.eleccion = IntVar()
-        self.opc1 = ttk.Radiobutton(self.frm, text="Agregar Dispositivo", value=1, variable=self.eleccion)
+        self.opc1 = ttk.Radiobutton(self.frm, text="Agregar Dispositivo", value=1, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
         self.opc1.grid(column=0, row=2)
-        self.opc2 = ttk.Radiobutton(self.frm, text="Cambiar Informacion del Dispositivo", value=2, variable=self.eleccion)
+        self.opc2 = ttk.Radiobutton(self.frm, text="Cambiar Informacion del Dispositivo", value=2, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
         self.opc2.grid(column=0, row=3)
-        self.opc3 = ttk.Radiobutton(self.frm, text="Eliminar Dispositivo", value=3, variable=self.eleccion)
+        self.opc3 = ttk.Radiobutton(self.frm, text="Eliminar Dispositivo", value=3, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
         self.opc3.grid(column=0, row=4)
-        self.opc4 = ttk.Radiobutton(self.frm, text="Generar Reporte", value=4, variable=self.eleccion)
+        self.opc4 = ttk.Radiobutton(self.frm, text="Generar Reporte", value=4, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
         self.opc4.grid(column=0, row=5)
+        self.btn1 = ttk.Button(self.frm, text="Seleccionar", command=lambda: self.seleccion(), state=DISABLED)
+        self.btn1.grid(column=0, row=6)
         self.menu.mainloop()
+
+    def seleccion(self):
+        if self.eleccion.get() == 1:
+            print("xd")
 
 
 main = Main()
-"""
-def seleccion(sel):
-    if sel == 1:
-        print("A")
-    elif sel == 2:
-        print("C")
-    elif sel == 3:
-        print("E")
-    else:
-        print("R")
-
-    mmenu.destroy()
-    adev.mainloop()
-elecButton(frm, text="Continuar", command=lambda: seleccion(eleccion.get())).grid(column=0, row=6)
-
-# Agregar Dispositivo
-adev = Tk()
-adev.title("Agregar Dispositivo")
-adev.geometry("700x450")
-adev.resizable(False, False)
-frm1 = ttk.Frame(adev, padding=10)
-
-
-# Default
-mmenu.mainloop()"""
-
