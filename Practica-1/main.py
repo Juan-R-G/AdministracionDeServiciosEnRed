d021a8782610c1c6f3f94db9e316012bd4d6af68
Juan <juan.0403.linux@gmail.com>
2023-03-21 21:47:44 -0600

    Modulo Principal

--- Practica-1/main.py
+++ Practica-1/main.py
@@ -1,4 +1,31 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
 
-mmenu = tk.Tk()
+def seleccion(sel):
+    if sel == 1:
+        print("A")
+    elif sel == 2:
+        print("C")
+    elif sel == 3:
+        print("E")
+    else:
+        print("R")
+
+mmenu = Tk()
+mmenu.title("Inicio")
+mmenu.geometry("700x450")
+mmenu.resizable(False, False)
+frm = ttk.Frame(mmenu, padding=10)
+frm.grid()
+info = "Sistema de Administracion de Red\nPractica 1 - Adquisicion de Informacion\nJuan Roldan Gomez 4CM14 2020630462"
+ttk.Label(frm, text=info).grid(column=0, row=0)
+ttk.Label(frm, text="Escoja una opcion:").grid(column=0, row=1)
+eleccion = IntVar()
+ttk.Radiobutton(frm, text="Agregar Dispositivo", value=1, variable=eleccion).grid(column=0, row=2)
+ttk.Radiobutton(frm, text="Cambiar Informacion del Dispositivo", value=2, variable=eleccion).grid(column=0, row=3)
+ttk.Radiobutton(frm, text="Eliminar Dispositivo", value=3, variable=eleccion).grid(column=0, row=4)
+ttk.Radiobutton(frm, text="Generar Reporte", value=4, variable=eleccion).grid(column=0, row=5)
+ttk.Button(frm, text="Continuar", command=lambda: print(eleccion.get())).grid(column=0, row=6)
+
 mmenu.mainloop()
+
