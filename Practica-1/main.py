41bc7c94e34377a1413f968666f4dabcbbcd7d2c
Juan <juan.0403.linux@gmail.com>
2023-03-21 22:37:20 -0600

    Modulo Principal

--- Practica-1/main.py
+++ Practica-1/main.py
@@ -2,6 +2,33 @@ from tkinter import *
 from tkinter import ttk
 
 
+class Main:
+    def __init__(self):
+        self.menu = Tk()
+        self.menu.title("Inicio")
+        self.menu.geometry("750x440")
+        self.menu.resizable(False, False)
+        self.frm = ttk.Frame(self.menu, padding=10)
+        self.frm.grid()
+        self.info = "Sistema de Administracion de Red\nPractica 1 - Adquisicion de Informacion\nJuan Roldan Gomez\t\t4CM14\t\t2020630462"
+        self.lbl1 = ttk.Label(self.frm, text=self.info)
+        self.lbl1.grid(column=0, row=0)
+        self.lbl2 = ttk.Label(self.frm, text="Escoja una opción:")
+        self.lbl2.grid(column=0, row=1)
+        self.eleccion = IntVar()
+        self.opc1 = ttk.Radiobutton(self.frm, text="Agregar Dispositivo", value=1, variable=self.eleccion)
+        self.opc1.grid(column=0, row=2)
+        self.opc2 = ttk.Radiobutton(self.frm, text="Cambiar Informacion del Dispositivo", value=2, variable=self.eleccion)
+        self.opc2.grid(column=0, row=3)
+        self.opc3 = ttk.Radiobutton(self.frm, text="Eliminar Dispositivo", value=3, variable=self.eleccion)
+        self.opc3.grid(column=0, row=4)
+        self.opc4 = ttk.Radiobutton(self.frm, text="Generar Reporte", value=4, variable=self.eleccion)
+        self.opc4.grid(column=0, row=5)
+        self.menu.mainloop()
+
+
+main = Main()
+"""
 def seleccion(sel):
     if sel == 1:
         print("A")
@@ -12,22 +39,18 @@ def seleccion(sel):
     else:
         print("R")
 
+    mmenu.destroy()
+    adev.mainloop()
+elecButton(frm, text="Continuar", command=lambda: seleccion(eleccion.get())).grid(column=0, row=6)
 
-mmenu = Tk()
-mmenu.title("Inicio")
-mmenu.geometry("700x450")
-mmenu.resizable(False, False)
-frm = ttk.Frame(mmenu, padding=10)
-frm.grid()
-info = "Sistema de Administracion de Red\nPractica 1 - Adquisicion de Informacion\nJuan Roldan Gomez 4CM14 2020630462"
-ttk.Label(frm, text=info).grid(column=0, row=0)
-ttk.Label(frm, text="Escoja una opcion:").grid(column=0, row=1)
-eleccion = IntVar()
-ttk.Radiobutton(frm, text="Agregar Dispositivo", value=1, variable=eleccion).grid(column=0, row=2)
-ttk.Radiobutton(frm, text="Cambiar Informacion del Dispositivo", value=2, variable=eleccion).grid(column=0, row=3)
-ttk.Radiobutton(frm, text="Eliminar Dispositivo", value=3, variable=eleccion).grid(column=0, row=4)
-ttk.Radiobutton(frm, text="Generar Reporte", value=4, variable=eleccion).grid(column=0, row=5)
-ttk.Button(frm, text="Continuar", command=lambda: seleccion(eleccion.get())).grid(column=0, row=6)
+# Agregar Dispositivo
+adev = Tk()
+adev.title("Agregar Dispositivo")
+adev.geometry("700x450")
+adev.resizable(False, False)
+frm1 = ttk.Frame(adev, padding=10)
 
-mmenu.mainloop()
+
+# Default
+mmenu.mainloop()"""
 
