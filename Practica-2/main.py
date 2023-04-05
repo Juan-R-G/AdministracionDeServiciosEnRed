d16ea037a62cfc23bd4e3188028b37888c995653
Juan <juan.0403.linux@gmail.com>
2023-04-05 10:19:41 -0600

    Practica 2

---
+++ Practica-2/main.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from tkinter import ttk
+
+
+class Main:
+    def __init__(self):
+        self.menu = Tk()
+        self.menu.title("Inicio")
+        self.menu.resizable(False, False)
+        self.frm = ttk.Frame(self.menu, padding=15)
+        self.frm.grid()
+        self.info = "Sistemade Administracion de Contabilidad\nPractica 2 - Modulo de Administracion de Contabilidad\nJuan Roldan Gomez\t\t4CM14\t\t2020630462"
+        self.lbl1 = ttk.Label(self.frm, text=self.info)
+        self.lbl1.grid(column=0, row=0)
+        self.lbl2 = ttk.Label(self.frm, text="Escoja una opcion:")
+        self.lbl2.grid(column=0, row=1)
+        self.eleccion = IntVar()
+        self.opc1 = ttk.Radiobutton(self.frm, text="Agregar Dispositivo", value=1, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
+        self.opc1.grid(column=0, row=2)
+        self.opc2 = ttk.Radiobutton(self.frm, text="Cambiar Informacion del Dispositivo", value=2, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
+        self.opc2.grid(column=0, row=3)
+        self.opc3 = ttk.Radiobutton(self.frm, text="Eliminar Dispositivo", value=3, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
+        self.opc3.grid(column=0, row=4)
+        self.opc4 = ttk.Radiobutton(self.frm, text="Generar Reporte", value=4, variable=self.eleccion, command=lambda: self.btn1.state(["!disabled"]))
+        self.opc4.grid(column=0, row=5)
+        self.btn1 = ttk.Button(self.frm, text="Seleccionar", command=lambda: self.seleccion(), state=DISABLED)
+        self.btn1.grid(column=0, row=6)
+        self.menu.mainloop()
+
+    def seleccion(self):
+        if self.eleccion.get() == 1:
+            self.menu.destroy()
+            Main()
+        elif self.eleccion.get() == 2:
+            self.menu.destroy()
+            Main()
+        elif self.eleccion.get() == 3:
+            self.menu.destroy()
+            Main()
+        else:
+            self.menu.destroy()
+            Main()
+
+
+Main()
