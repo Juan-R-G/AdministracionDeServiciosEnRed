4a55a31290194bbb47fa12c4a046a84865d98e8f
Juan <juan.0403.linux@gmail.com>
2023-04-05 12:50:11 -0600

    Practica 2

--- Practica-2/main.py
+++ Practica-2/main.py
@@ -11,7 +11,7 @@ class Main:
         self.frm = ttk.Frame(self.menu, padding=15)
         self.frm.grid()
         self.info = "Sistemade Administracion de Contabilidad\nPractica 2 - Modulo de Administracion de Contabilidad\nJuan Roldan Gomez\t\t4CM14\t\t2020630462"
-        self.lbl1 = ttk.Label(self.frm, text=self.info)
+        self.lbl1 = ttk.Label(self.frm, text=self.info) # width: anchor: center
         self.lbl1.grid(column=0, row=0)
         self.lbl2 = ttk.Label(self.frm, text="Escoja una opcion:")
         self.lbl2.grid(column=0, row=1)
