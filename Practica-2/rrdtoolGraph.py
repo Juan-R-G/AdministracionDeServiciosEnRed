d33d51927b0bddc9977c024a7a84280563710431
Juan <juan.0403.linux@gmail.com>
2023-04-07 21:26:40 -0600

    Practica 2

---
+++ Practica-2/rrdtoolGraph.py
@@ -0,0 +1,15 @@
+import rrdtool
+import time
+
+
+def grafica(filename, time_start, time_end, title, var, db, name):
+    try:
+        rrdtool.graph(filename, "--start", str(time_start), "--end", str(time_end),
+                      "--vertical-label=Bytes/s", "--title=" + title,
+                      "DEF:variable=" + db + ":" + var + ":AVERAGE",
+                      "CDEF:escala=variable,8,*",
+                      "LINE3:escala#0000FF:" + name)
+        return "OK"
+    except Exception as e:
+        print(e)
+        return "error"
