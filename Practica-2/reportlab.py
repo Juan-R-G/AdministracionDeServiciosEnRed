a37fb0426f315a7917b79b3bc8a122df62fac01c
Juan <juan.0403.linux@gmail.com>
2023-04-16 15:53:27 -0600

    Practica 2

--- Practica-2/reportlab.py
+++ Practica-2/reportlab.py
@@ -1,6 +1,6 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 from reportlab.lib import colors
 
 
@@ -15,9 +15,36 @@ def reporte1(path, title, info, image, table):
             y -= 25
         y -= 40
         mycanvas.setFont('Helvetica', 14)
-        mycanvas.drawImage(image)
+        mycanvas.drawImage(image, x+290, y-95, width=235, height=137)
+        for i in info:
+            mycanvas.drawString(x, y, i)
+            y -= 23
+        y -= 100
+        t = Table(table)
+        t.setStyle(TableStyle([
+            ('ALIGN', (0,0), (-1, -1), 'CENTRE'),
+            ('FONT', (0, 0), (-1, -1), 'Helvetica'),
+            ('FONTSIZE', (0, 0), (-1, -1), 14),
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+            ('TEXTCOLOR', (1, 1), (-1, -1), colors.mediumblue),
+            ('LINEBELOW', (0, 0), (-1, 0), colors.black),
+            ('LINEAFTER', (0, 0), (0, -1), colors.black),
+            ('LINEBELOW', (0, 1), (-1, -2), colors.black)
+        ]))
+        t.wrapOn(mycanvas, 150, 100)
+        t.drawOn(mycanvas, x, y)
         mycanvas.save()
         return "Se ha creado el reporte en formato PDF y se ha guardado en la carpeta 'Reportes'!"
+    except Exception as e:
+        print(e)
+        return "Error al generar el reporte: " + e.args[0]
+
+
+def reporte2(path):
+    try:
+        mydoc = SimpleDocTemplate(path, pagesize=letter)
+
+        mydoc.build()
     except Exception as e:
         print(e)
         return "Error al generar el reporte: " + e.args[0]
