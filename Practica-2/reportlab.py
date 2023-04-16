9bb40d586fe4db7ace3f50a110902d96ff9765bf
Juan <juan.0403.linux@gmail.com>
2023-04-16 15:38:59 -0600

    Practica 2

--- Practica-2/reportlab.py
+++ Practica-2/reportlab.py
@@ -7,6 +7,15 @@ from reportlab.lib import colors
 def reporte1(path, title, info, image, table):
     try:
         mycanvas = canvas.Canvas(path, pagesize=letter)
+        x = 50
+        y = 742
+        mycanvas.setFont('Helvetica', 18)
+        for t in title:
+            mycanvas.drawString(x+110, y, t)
+            y -= 25
+        y -= 40
+        mycanvas.setFont('Helvetica', 14)
+        mycanvas.drawImage(image)
         mycanvas.save()
         return "Se ha creado el reporte en formato PDF y se ha guardado en la carpeta 'Reportes'!"
     except Exception as e:
