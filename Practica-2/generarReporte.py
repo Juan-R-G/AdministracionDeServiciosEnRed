0f6ad823f63717f3bd27e5493a04e033a3d201a0
Juan <juan.0403.linux@gmail.com>
2023-04-16 20:44:17 -0600

    Practica 2

--- Practica-2/generarReporte.py
+++ Practica-2/generarReporte.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 import os
+from datetime import datetime
 from report_lab import *
 
 
@@ -25,43 +26,145 @@ class Report:
             self.lbl1 = ttk.Label(self.frm, text="Seleccione un Dispositivo:")
         else:
             self.lbl1 = ttk.Label(self.frm, text="No hay dispositivos guardados...")
-        self.lbl1.grid(columnspan=3, row=0)
+        self.lbl1.grid(columnspan=6, row=0)
         self.eleccion = StringVar()
         self.radioButtons = []
-        self.c = 0
+        self.row = 0
         if self.flag:
             for name in self.devices:
                 self.radioButtons.append(ttk.Radiobutton(self.frm, text=name, value=name, variable=self.eleccion, command=lambda: self.dispositivo()))
-                self.c += 1
-                self.radioButtons[self.c-1].grid(columnspan=3, row=self.c)
+                self.row += 1
+                self.radioButtons[self.row-1].grid(columnspan=6, row=self.row)
         self.lbl2 = ttk.Label()
         self.tipoReporte = IntVar()
         self.radioButtons1 = []
         self.radioButtons1.append(ttk.Radiobutton(self.frm, text="Reporte General", value=1, variable=self.tipoReporte, command=lambda: self.tipo1()))
         self.radioButtons1.append(ttk.Radiobutton(self.frm, text="Reporte de Contabilidad", value=2, variable=self.tipoReporte, command=lambda: self.tipo2()))
+        self.lbl3 = ttk.Label()
+        self.lbl4 = ttk.Label()
+        self.lbl5 = ttk.Label()
+        self.lbl6 = ttk.Label()
+        self.lbl7 = ttk.Label()
+        self.lbl8 = ttk.Label()
+        self.lbl9 = ttk.Label()
+        self.lbl10 = ttk.Label()
+        self.spin1 = ttk.Spinbox()
+        self.spin2 = ttk.Spinbox()
+        self.spin3 = ttk.Spinbox()
+        self.spin4 = ttk.Spinbox()
+        self.spin5 = ttk.Spinbox()
+        self.spin6 = ttk.Spinbox()
+        self.lbl11 = ttk.Label()
+        self.lbl12 = ttk.Label()
+        self.lbl13 = ttk.Label()
+        self.lbl14 = ttk.Label()
+        self.lbl15 = ttk.Label()
+        self.lbl16 = ttk.Label()
+        self.spin7 = ttk.Spinbox()
+        self.spin8 = ttk.Spinbox()
+        self.spin9 = ttk.Spinbox()
+        self.spin10 = ttk.Spinbox()
+        self.spin11 = ttk.Spinbox()
+        self.spin12 = ttk.Spinbox()
         self.btn1 = ttk.Button()
         if self.flag:
             self.btn1 = ttk.Button(self.frm, text="Generar", command=lambda: self.generar(), state=DISABLED)
-            self.btn1.grid(columnspan=3, row=self.c+10)
+            self.btn1.grid(columnspan=6, row=self.row+9)
         else:
             self.btn1 = ttk.Button(self.frm, text="Continuar", command=lambda: self.menu.destroy())
-            self.btn1.grid(columnspan=3, row=1)
+            self.btn1.grid(columnspan=6, row=self.row+1)
         self.menu.mainloop()
 
     def dispositivo(self):
         self.lbl2 = ttk.Label(self.frm, text="Seleccione el reporte a generar:")
-        self.c = len(self.devices) + 1
-        self.lbl2.grid(columnspan=3, row=self.c)
-        self.c += 1
-        self.radioButtons1[0].grid(columnspan=3, row=self.c)
-        self.c += 1
-        self.radioButtons1[1].grid(columnspan=3, row=self.c)
+        self.row = len(self.devices) + 1
+        self.lbl2.grid(columnspan=6, row=self.row)
+        self.row += 1
+        self.radioButtons1[0].grid(columnspan=6, row=self.row)
+        self.row += 1
+        self.radioButtons1[1].grid(columnspan=6, row=self.row)
 
     def tipo1(self):
-        pass
+        self.lbl3.destroy()
+        self.lbl4.destroy()
+        self.lbl5.destroy()
+        self.lbl6.destroy()
+        self.lbl7.destroy()
+        self.lbl8.destroy()
+        self.lbl9.destroy()
+        self.lbl10.destroy()
+        self.spin1.destroy()
+        self.spin2.destroy()
+        self.spin3.destroy()
+        self.spin4.destroy()
+        self.spin5.destroy()
+        self.spin6.destroy()
+        self.lbl11.destroy()
+        self.lbl12.destroy()
+        self.lbl13.destroy()
+        self.lbl14.destroy()
+        self.lbl15.destroy()
+        self.lbl16.destroy()
+        self.spin7.destroy()
+        self.spin8.destroy()
+        self.spin9.destroy()
+        self.spin10.destroy()
+        self.spin11.destroy()
+        self.spin12.destroy()
+        self.btn1.state(["!disabled"])
 
     def tipo2(self):
-        pass
+        self.row = len(self.devices) + 4
+        self.lbl3 = ttk.Label(self.frm, text="Fecha Inicio")
+        self.lbl3.grid(columnspan=3, row=self.row)
+        self.lbl4 = ttk.Label(self.frm, text="Fecha Termino")
+        self.lbl4.grid(columnspan=3, row=self.row)
+        self.row += 1
+        self.lbl5 = ttk.Label(self.frm, text="Dia")
+        self.lbl5.grid(column=0, row=self.row)
+        self.lbl6 = ttk.Label(self.frm, text="Mes")
+        self.lbl6.grid(column=1, row=self.row)
+        self.lbl7 = ttk.Label(self.frm, text="Año")
+        self.lbl7.grid(column=2, row=self.row)
+        self.lbl8 = ttk.Label(self.frm, text="Dia")
+        self.lbl8.grid(column=3, row=self.row)
+        self.lbl9 = ttk.Label(self.frm, text="Mes")
+        self.lbl9.grid(column=4, row=self.row)
+        self.lbl10 = ttk.Label(self.frm, text="Año")
+        self.lbl10.grid(column=5, row=self.row)
+        self.row += 1
+        t = datetime.now()
+        self.spin1 = ttk.Spinbox(self.frm, from_=1, to=31, width=5)
+        self.spin1.insert(0, str(t.day))
+        self.spin1.grid(column=0, row=self.row)
+        self.spin2 = ttk.Spinbox(self.frm, from_=1, to=12, width=5)
+        self.spin2.insert(0, str(t.month))
+        self.spin2.grid(column=1, row=self.row)
+        self.spin3 = ttk.Spinbox(self.frm, from_=2022, to=2023, width=5)
+        self.spin3.insert(0, str(t.year))
+        self.spin3.grid(column=2, row=self.row)
+        self.spin4 = ttk.Spinbox(self.frm, from_=1, to=31, width=5)
+        self.spin4.insert(0, str(t.day))
+        self.spin4.grid(column=3, row=self.row)
+        self.spin5 = ttk.Spinbox(self.frm, from_=1, to=12, width=5)
+        self.spin5.insert(0, str(t.month))
+        self.spin5.grid(column=4, row=self.row)
+        self.spin6 = ttk.Spinbox(self.frm, from_=2022, to=2023, width=5)
+        self.spin6.insert(0, str(t.year))
+        self.spin6.grid(column=5, row=self.row)
+        self.row += 1
+        self.lbl11 = ttk.Label(self.frm, text="Hora")
+        self.lbl12 = ttk.Label()
+        self.lbl13 = ttk.Label()
+        self.lbl14 = ttk.Label()
+        self.lbl15 = ttk.Label()
+        self.lbl16 = ttk.Label()
+        self.spin7 = ttk.Spinbox()
+        self.spin8 = ttk.Spinbox()
+        self.spin9 = ttk.Spinbox()
+        self.spin10 = ttk.Spinbox()
+        self.spin11 = ttk.Spinbox()
+        self.spin12 = ttk.Spinbox()
 
     def generar(self):
         pass
