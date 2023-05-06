fa9af66b8e9044a0b6cda0b61f22f45b6accbaf2
Juan <juan.0403.linux@gmail.com>
2023-05-05 18:48:51 -0600

    Practica 3

--- Practica-3/rrdtoolGraph.py
+++ Practica-3/rrdtoolGraph.py
@@ -14,7 +14,7 @@ def grafica(filename, time_start, time_end, title, unit, var, db, op, name):
         return "Error al generar la grafica: " + e.args[0]
 
 
-def grafica1(filename, time_start, time_end, title, unit, var, db):  # Uso del Procesador 1
+def grafica1(filename, time_start, time_end, db):  # Uso del Procesador 1
     try:
         rrdtool.graph(filename, "--start", str(time_start), "--end", str(time_end),
                       "--vertical-label=Porcentaje", "--lower-limit", "0", "--upper-limit", "100",
@@ -35,10 +35,11 @@ def grafica1(filename, time_start, time_end, title, unit, var, db):  # Uso del P
         return "Error al generar la grafica del uso del CPU 1: " + e.args[0]
 
 
-def grafica2(filename, time_start, time_end, title, unit, var, db, name):  # RAM
+def grafica2(filename, time_start, time_end, db):  # Uso de la RAM
     try:
         rrdtool.graph(filename, "--start", str(time_start), "--end", str(time_end),
-                      "--vertical-label=" + unit, "--title=" + title,
+                      "--vertical-label=Porcentaje", "--lower-limit", "0", "--upper-limit", "100",
+                      "--title=Uso de la RAM",
                       "DEF:var1=" + db + ":" + var[0] + ":AVERAGE",
                       "DEF:var2=" + db + ":" + var[1] + ":AVERAGE",
                       "CDEF:var3=var1,100,*," + var[2] + ",/")
