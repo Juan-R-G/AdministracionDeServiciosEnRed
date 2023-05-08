62a31c002803d1b9dd976a5ea60056e7594d7168
Juan <juan.0403.linux@gmail.com>
2023-05-07 18:01:16 -0600

    Practica 3

--- Practica-3/enviarCorreo.py
+++ Practica-3/enviarCorreo.py
@@ -1,2 +1,28 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+
+
 def enviar(asunto, mensaje, imagen):
-    pass
+    de = "juan.0403.0112@gmail.com"
+    para = "juan.0403.0112@gmail.com"
+    mailserver = "smtp.gmail.com: 587"
+    contrasena = ""
+    try:
+        msg = MIMEMultipart()
+        msg['Subject'] = asunto
+        msg['From'] = de
+        msg['To'] = para
+        f = open(imagen, "rb")
+        img = MIMEImage(f.read())
+        f.close()
+        msg.attach(img)
+        s = smtplib.SMTP(mailserver)
+        s.starttls()
+        s.login(de, contrasena)
+        s.sendmail(de, para, msg.as_string())
+        s.quit()
+
+        print("Se envio exitosamente el correo de notificacion!")
+    except Exception as e:
+        print("Error al enviar el correo: " + e.args[0])
